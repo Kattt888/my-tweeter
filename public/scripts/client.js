92d55af876256e5567ce1e6ef4982212c5d88589
Katrina <katrinabyrd88@gmail.com>
2024-12-05 00:36:12 -0500

    created function loadTweets to fetch tweets from server using AJAX, applied timeago to dislay timestamp

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,89 +4,80 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-// Function to render tweets by iterating through the tweets array
-const renderTweets = function(tweets) {
-  $('#tweets-container').empty(); // Prevent duplicates
-  tweets.forEach(tweet => {
-    const $tweetElement = createTweetElement(tweet);
-    $('#tweets-container').prepend($tweetElement); // Prepend for reverse chronological order
-  });
-};
-
-// Function to create a new tweet element from the tweet object
-const createTweetElement = function(tweet) {
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <div class="user-info">
-          <img src="${tweet.user.avatars}" alt="${tweet.user.name}'s avatar">
-          <span class="name">${tweet.user.name}</span>
-          <span class="handle">${tweet.user.handle}</span>
-        </div>
-      </header>
-      <p class="tweet-text">${tweet.content.text}</p>
-      <footer>
-        <time>${new Date(tweet.created_at).toLocaleString()}</time>
-        <div class="icons">
-          <i class="fa fa-flag"></i>
-          <i class="fa fa-retweet"></i>
-          <i class="fa fa-heart"></i>
-        </div>
-      </footer>
-    </article>
-  `);
-  return $tweet;
-};
-
-// Render the initial set of tweets (testing)
-renderTweets(data);
-
 $(document).ready(function() {
-  // Event listener for tweet form submission
-  $('form').on('submit', function(event) {
-    event.preventDefault(); // Prevent default form submission
+  
+  const renderTweets = function(tweets) {
+    $('#tweets-container').empty();
 
-    // Serialize form data
+  
+    tweets.forEach(tweet => {
+      const $tweetElement = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweetElement);
+    });
+  };
+
+  const createTweetElement = function(tweet) {
+    const $tweet = $(`
+      <article class="tweet">
+        <header>
+          <div class="user-info">
+            <img src="${tweet.user.avatars}" alt="${tweet.user.name}'s avatar">
+            <span class="name">${tweet.user.name}</span>
+            <span class="handle">${tweet.user.handle}</span>
+          </div>
+        </header>
+        <p class="tweet-text">${tweet.content.text}</p>
+        <footer>
+          <time>${timeago.format(tweet.created_at)}</time>
+          <div class="icons">
+            <i class="fa fa-flag"></i>
+            <i class="fa fa-retweet"></i>
+            <i class="fa fa-heart"></i>
+          </div>
+        </footer>
+      </article>
+    `);
+    return $tweet;
+  };
+
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function(response) {
+        renderTweets(response);
+      },
+      error: function(err) {
+        console.error('Error fetching tweets:', err);
+      }
+    });
+  };
+
+  loadTweets();
+
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+
     const formData = $(this).serialize();
 
-    // Send the AJAX POST request
+    const tweetText = $('#tweet-text').val().trim();
+    if (tweetText === '') {
+      alert("Tweet cannot be empty!");
+      return;
+    }
+    if (tweetText.length > 140) {
+      alert("Tweet exceeds the 140 character limit!");
+      return;
+    }
+
     $.ajax({
       type: 'POST',
       url: '/tweets',
       data: formData,
       success: function() {
-        console.log("Tweet submitted successfully!");
-
-        // After submitting the form, clear the textarea and reset the counter
         $('#tweet-text').val('');
-        $('.counter').text('140');
-
-        // Load tweets again to display the new tweet
+        $('.counter').text(140);
         loadTweets();
       },
       error: function(error) {
@@ -94,22 +85,4 @@ $(document).ready(function() {
       }
     });
   });
-
-  // Function to load tweets from the server using AJAX GET request
-  const loadTweets = function() {
-    $.ajax({
-      type: 'GET',
-      url: '/tweets',
-      success: function(tweets) {
-        // Render the tweets after fetching them
-        renderTweets(tweets);
-      },
-      error: function(error) {
-        console.error("Error fetching tweets:", error);
-      }
-    });
-  };
-
-  // Load tweets when the page is loaded
-  loadTweets();
 });
